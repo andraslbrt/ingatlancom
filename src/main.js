35504c16b514f4268ee95f0dc9bba467ba2fae63
andraslbrt <albert.andras@gmail.com>
2022-06-23 10:16:55 +0200

    Add bootstrap and app.scss

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import { createApp } from 'vue';
+import {
+  BIconListUl, BIconStarFill, BIconStar, BIconArrowLeft, BIconCalendarEvent, BIconList, BIconChevronLeft,
+} from 'bootstrap-icons-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+
+const app = createApp(App);
+
+app.use(store)
+  .use(router)
+  .component('BIconListUl', BIconListUl)
+  .component('BIconStarFill', BIconStarFill)
+  .component('BIconStar', BIconStar)
+  .component('BIconArrowLeft', BIconArrowLeft)
+  .component('BIconCalendarEvent', BIconCalendarEvent)
+  .component('BIconList', BIconList)
+  .component('BIconChevronLeft', BIconChevronLeft)
+  .mount('#app');
